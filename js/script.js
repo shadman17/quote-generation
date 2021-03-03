d0131461c7b3a1a46c54e1f2992aff2b1d5508f9
shadman17 <shadman.wasif@gmail.com>
2021-03-03 21:07:02 +0600

    Final Commit

--- js/script.js
+++ js/script.js
@@ -4,17 +4,17 @@ const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
-
+const themeBtn = document.getElementById('change-theme');
 // Loading Spinner Shown
 
-function showLoadingSpin(){
+function showLoadingSpin() {
   quoteContainer.hidden = true;
   loader.hidden = false;
 }
 
 // Remove Loading Spinner
-function removeLoadingSpin(){
-  if(loader.hidden === false){
+function removeLoadingSpin() {
+  if (loader.hidden === false) {
     quoteContainer.hidden = false;
     loader.hidden = true;
   }
@@ -33,25 +33,28 @@ async function getQuote() {
 
     // Check if Author field is blank and replace it with 'Unknown'
 
-    if (data.quoteAuthor === ''){
+    if (data.quoteAuthor === '') {
       authorText.innerText = 'Unknown';
-    } else{
+    } else {
       authorText.innerText = data.quoteAuthor;
     }
 
     // Dynamically reduce font size for long quotes
-    if (data.quoteText.length > 20){
+    if (data.quoteText.length > 20) {
       quoteText.classList.add('long-quote')
-    } else { 
+    } else {
       quoteText.classList.remove('long-quote')
     }
     quoteText.innerText = data.quoteText;
 
     // Stop Loading, Show Quote
     removeLoadingSpin();
+
   } catch (error) {
     //console.log(error);
-    getQuote();
+    quoteText.innerText = `Something Went Wrong, Try Again Later`;
+    authorText.innerText = '';
+    //twitterBtn.hidden = true;
   }
 }
 
@@ -63,9 +66,29 @@ function tweetQuote() {
   window.open(twitterUrl, '_blank');
 }
 
+let count = 0;
+const changeTheme = () => {
+  const body = document.querySelector('body');
+  if (count%2 == 0) {
+    body.style.background = 'white'
+  } else {
+    body.style.background = 'black'
+  }
+  if(body.style.backgroundColor === 'white'){
+    body.style.backgroundColor = 'black';
+    count++;
+  }
+  else{
+    body.style.backgroundColor = 'white';
+    count++;
+  }
+}
+
+
 // Event Listeners
 newQuoteBtn.addEventListener('click', getQuote);
 twitterBtn.addEventListener('click', tweetQuote);
+themeBtn.addEventListener('click', changeTheme);
 
 // On Load
 getQuote();
